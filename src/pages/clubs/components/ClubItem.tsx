987559100dac1da8aca36587303a089f18e313d4
KWONKISEOK <kwonkiseok7@gmail.com>
2023-03-05 02:14:22 +0900

    * image optimization disabled

--- src/pages/clubs/components/ClubItem.tsx
+++ src/pages/clubs/components/ClubItem.tsx
@@ -1,5 +1,4 @@
 import { SubjectType } from "@/models/subject.type";
-import Image from "next/image";
 import React from "react";
 import { getSubjectObject } from "../[subject]";
 
@@ -18,7 +17,7 @@ const ClubItem: React.FC<ClubItemProps> = ({ logo, title, shortDesc, subject }) 
 
     return (
         <div onClick={handleClickItem} className="px-4 py-4 flex flex-row hover:bg-slate-100 rounded-2xl cursor-pointer">
-            <Image alt="logo" width={64} height={64} className="min-w-[64px] w-16 h-16 object-contain mr-6 rounded-lg" src={logo} />
+            <img alt="logo" className="min-w-[64px] w-16 h-16 object-contain mr-6 rounded-lg" src={logo} />
             <div className="flex flex-col">
                 <p className="font-bold text-xl mb-1 text-gray-700">{title}</p>
                 <p className="text-gray-500 mb-2">{shortDesc}</p>
