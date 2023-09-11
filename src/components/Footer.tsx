26a87d980844e908a10a96886dde598734b81476
MODORIAPPS <kwonkiseok7@gmail.com>
2023-09-11 18:17:10 +0900

    feat: 이미지 변경 및 기능 추가 - 동아리 배치도 이미지 갱신 - Footer 마지막 빌드타임 표기 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 
-interface FooterProps { }
-
-const Footer: React.FC<FooterProps> = () => {
-
-    const handleClickUnion = () => {
-        window.open("https://sejongclubunion.com");
-    };
-
-    const handleClickInstagram = () => {
-        window.open("https://www.instagram.com/sejong_clubunion/");
-    };
+const Footer: React.FC = () => {
 
     return (
         <div className="bg-[#f9fafb] pt-[20px] pb-[20px]">
@@ -27,13 +18,28 @@ const Footer: React.FC<FooterProps> = () => {
                 <p className="text-sm text-[#9A9A9A] mb-4">
                     세종대학교 총동아리연합회는 다양한 분야의 동아리를 연결하여 학내 문화와 창의성을 증진시키는 조직입니다.<br /><br />
                     05006 서울특별시 광진구 능동로 209 세종대학교 학생회관 408호<br />
-                    Copyright(C) 세종대학교 제 41대 총동아리연합회 라이트 All rights reserved <br />
+                    Copyright(C) 세종대학교 제 41대 총동아리연합회 라이트 All rights reserved<br />
+                    <span className="underline">{formatDate(new Date(process.env.NEXT_PUBLIC_LAST_BUILDTIME ?? "2002-01-01"))} Version</span>
                 </p>
-                <span onClick={handleClickUnion} className="text-sm font-bold text-[#9A9A9A] cursor-pointer underline mr-4">총동아리연합회 웹사이트</span>
-                <span onClick={handleClickInstagram} className="text-sm font-bold text-[#9A9A9A] cursor-pointer underline">인스타그램</span>
+                <Link target="_blank" href="https://sejongclubunion.com" className="text-sm font-bold text-[#9A9A9A] cursor-pointer underline mr-4">총동아리연합회 웹사이트</Link>
+                <Link target="_blank" href="https://www.instagram.com/sejong_clubunion" className="text-sm font-bold text-[#9A9A9A] cursor-pointer underline mr-4">인스타그램</Link>
             </div>
         </div>
     );
+};
+
+function formatDate(d: Date) {
+    const months = [
+        'January', 'February', 'March', 'April', 'May',
+        'June', 'July', 'August', 'September', 'October',
+        'November', 'December'
+    ];
+
+    const year = d.getFullYear();
+    const month = months[d.getMonth()];
+    const day = d.getDate();
+
+    return `${year} ${month} ${day}`;
 };
 
 export default Footer;
